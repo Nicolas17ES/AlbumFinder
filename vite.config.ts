a6134ebdd74d8f08b654150e7e552343c1fd2a55
Nicolas17ES <83025990+Nicolas17ES@users.noreply.github.com>
2024-10-16 19:30:27 +0200

    changed port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Required for Railway or other cloud providers
+    port: Number(process.env.PORT) || 5173, // Use the PORT environment variable or fallback to 5173
+  },
+});
